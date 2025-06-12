d16b7e8bdba30d4da3dcef632f2d301abf64717b
mariaalexandrazamfir <mariaa40@yahoo.com>
2025-06-12 15:53:49 +0100

    Add FinanceManager service class core operations

--- src/main/java/com/mpfm/service/FinanceManager.java
+++ src/main/java/com/mpfm/service/FinanceManager.java
@@ -1,4 +1,29 @@
 package com.mpfm.service;
 
+import com.mpfm.model.Entry;
+import com.mpfm.model.EntryType;
+import com.mpfm.repository.EntryRepository;
+
+import java.util.List;
+
 public class FinanceManager {
+
+    private final EntryRepository repository;
+
+    public FinanceManager(EntryRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addEntry(EntryType type, double amount, String description) {
+        Entry entry = new Entry(type, amount, description, java.time.LocalDate.now());
+        repository.addEntry(entry);
+    }
+
+    public List<Entry> getAllEntries() {
+        return repository.getAllEntries();
+    }
+
+    public List<Entry> getEntriesByType(EntryType type) {
+        return repository.findByType(type);
+    }
 }
